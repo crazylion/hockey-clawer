ec942fd4c8683ee3519d400db3b0fb1d75c43ce3
Meng-yang Lee <mengyang@larvata.tw>
2021-11-14 21:50:07 +0800

    initial commmit

---
+++ lib/hockey/clawer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "clawer/version"
+
+module Hockey
+  module Clawer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/hockey/clawer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hockey::Clawer do
+  it "has a version number" do
+    expect(Hockey::Clawer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
